44ac54645e2dfef55bd008077679a55cec1d6f33
k3ndr1c <ken_dric@hotmail.com>
2020-10-20 16:09:59 -0400

    Updates

---
+++ precache-manifest.b0e716b903d5a052fca03d707b381ffa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cff29fccc1a91c4d3695f84405dd0446",
+    "url": "/colorblocks/index.html"
+  },
+  {
+    "revision": "d6203a1ab225e411261c",
+    "url": "/colorblocks/static/css/main.d620f3ee.chunk.css"
+  },
+  {
+    "revision": "f372fc422f80cb79f334",
+    "url": "/colorblocks/static/js/2.14c02948.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/colorblocks/static/js/2.14c02948.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6203a1ab225e411261c",
+    "url": "/colorblocks/static/js/main.f48aebff.chunk.js"
+  },
+  {
+    "revision": "b3c36792f0eef3e897bb",
+    "url": "/colorblocks/static/js/runtime-main.469e1197.js"
+  }
+]);
